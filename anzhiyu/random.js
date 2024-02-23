2bca30ebd8a96736d36de78ae282549b510bf81d
Mag <78414829+magblank@users.noreply.github.com>
2024-02-23 20:18:02 +0800

    Site updated: 2024-02-23 20:18:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/23/本站说明及常见问题.html","2024/02/22/ginka-pc-krkr-官中版-全cg存档.html","2024/02/23/atri-my-dear-moments-pc-krkr官中版.html"];function toRandomPost(){
+var posts=["2024/02/23/本站说明及常见问题.html","2024/02/23/atri-my-dear-moments-pc-krkr官中版.html","2024/02/22/ginka-pc-krkr-官中版-全cg存档.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
