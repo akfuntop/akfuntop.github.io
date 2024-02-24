e00b344836a28e6c32cf64e01a87c4a892e410f2
akfuntop <160688478+akfuntop@users.noreply.github.com>
2024-02-24 07:50:28 +0000

    修改部署流程 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/23/本站说明及常见问题.html","2024/02/23/atri-my-dear-moments-pc-krkr官中版.html","2024/02/22/ginka-pc-krkr-官中版-全cg存档.html"];function toRandomPost(){
+var posts=["2024/02/23/本站说明及常见问题.html","2024/02/24/galgame-7day-与你共度的七日间-pc-krkr.html","2024/02/24/兽耳娘大作战-animal☆panic-pc汉化全cg.html","2024/02/23/atri-my-dear-moments-pc-krkr官中版.html","2024/02/22/ginka-pc-krkr-官中版-全cg存档.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
