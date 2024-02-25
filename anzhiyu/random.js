3f31b2b96b5ac56952591bdef148be6e5a16516e
akfuntop <160688478+akfuntop@users.noreply.github.com>
2024-02-25 09:22:19 +0000

    约定的轨迹 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/23/本站说明及常见问题.html","2024/02/24/galgame-7day-与你共度的七日间-pc-krkr.html","2024/02/24/兽耳娘大作战-animal☆panic-pc汉化全cg.html","2024/02/23/atri-my-dear-moments-pc-krkr官中版.html","2024/02/24/巧克甜恋1-pc汉化版-全cg存档.html","2024/02/25/巧克甜恋2-pc官中.html","2024/02/22/ginka-pc-krkr-官中版-全cg存档.html"];function toRandomPost(){
+var posts=["2024/02/23/本站说明及常见问题.html","2024/02/24/7day-与你共度的七日间-pc-krkr.html","2024/02/24/兽耳娘大作战-animal☆panic-pc汉化全cg.html","2024/02/23/atri-my-dear-moments-pc-krkr官中版.html","2024/02/24/巧克甜恋1-pc汉化版-全cg存档.html","2024/02/25/巧克甜恋2-pc官中.html","2024/02/22/ginka-pc-krkr-官中版-全cg存档.html","2024/02/25/qualia-～约定的轨迹～-官中-pc-tyranor版.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
